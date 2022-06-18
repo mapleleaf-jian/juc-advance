2203e986832aabde22b2c7c9370e219289ad8d89
mapleleaf-jian <1028517695@qq.com>
2022-06-18 21:23:47 +0800

    CompletableFuture 常用方法3组

--- src/jucadv/methods/Demo3.java
+++ src/jucadv/methods/Demo3.java
@@ -3,6 +3,7 @@ package jucadv.methods;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author maple
@@ -10,6 +11,26 @@ import java.util.concurrent.Executors;
  */
 public class Demo3 {
     public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        try {
+            CompletableFuture<Void> completableFuture = CompletableFuture.supplyAsync(() -> {
+                try { TimeUnit.MILLISECONDS.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
+                System.out.println(Thread.currentThread().getName() + "执行1号任务");
+                return 11;
+            }, threadPool).thenRun(() -> {
+                try { TimeUnit.MILLISECONDS.sleep(200); } catch (InterruptedException e) { e.printStackTrace(); }
+                System.out.println(Thread.currentThread().getName() + "执行2号任务");
+            }).thenRun(() -> {
+                try { TimeUnit.MILLISECONDS.sleep(200); } catch (InterruptedException e) { e.printStackTrace(); }
+                System.out.println(Thread.currentThread().getName() + "执行3号任务");
+            });
+            System.out.println(completableFuture.join());
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+
+    private static void extracted1() {
         ExecutorService threadPool = Executors.newSingleThreadExecutor();
         System.out.println(CompletableFuture.supplyAsync(() -> 1).thenRun(() -> {}).join()); // null
         System.out.println(CompletableFuture.supplyAsync(() -> 1).thenApply(f -> f + 2).join()); // 3
