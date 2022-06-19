23ca94c8807fbbfd604221db41b56acc09df81a5
mapleleaf-jian <1028517695@qq.com>
2022-06-19 10:48:47 +0800

    synchronized 反编译

---
+++ src/jucadv/synchro/Demo.java
@@ -0,0 +1,26 @@
+package jucadv.synchro;
+
+/**
+ * @author maple
+ * @create 2022-06-19 10:16
+ */
+public class Demo {
+//    private Object obj = new Object();
+//    public void m1() {
+//        synchronized (obj) {
+//
+//        }
+//    }
+
+    public synchronized void m2() {
+
+    }
+
+    public static synchronized void m3() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
