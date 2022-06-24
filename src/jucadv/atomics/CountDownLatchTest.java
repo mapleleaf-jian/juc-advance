2b3c91075722afd8b71ea61e2325c3b86020dcfb
mapleleaf-jian <1028517695@qq.com>
2022-06-24 13:47:50 +0800

    基本类型原子类

---
+++ src/jucadv/atomics/CountDownLatchTest.java
@@ -0,0 +1,37 @@
+package jucadv.atomics;
+
+import javax.naming.SizeLimitExceededException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author zcl2806
+ * @create 2022-06-24 11:34
+ */
+class MyNum {
+    public AtomicInteger atomicInteger = new AtomicInteger();
+    public void addPlus() {
+        atomicInteger.getAndIncrement();
+    }
+}
+public class CountDownLatchTest {
+    public static final int SIZE = 50;
+    public static void main(String[] args) throws InterruptedException {
+        MyNum myNum = new MyNum();
+        CountDownLatch countDownLatch = new CountDownLatch(SIZE);
+        for (int i = 0; i < SIZE; i++) {
+            new Thread(() -> {
+                try {
+                    for (int j = 0; j < 1000; j++) {
+                        myNum.addPlus();
+                    }
+                } finally {
+                    countDownLatch.countDown();
+                }
+            }, String.valueOf(i)).start();
+        }
+        countDownLatch.await();
+        System.out.println(myNum.atomicInteger.get()); // 50000
+    }
+}
